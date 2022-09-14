eae18491e8a769bd9177efb7f2911da85d4411e0
Joselyne M. Tran <tran3jm@dukes.jmu.edu>
2022-09-14 15:07:04 -0400

    More integration tests

--- p1/p1-lexer/src/p1-lexer.c
+++ p1/p1-lexer/src/p1-lexer.c
@@ -12,17 +12,17 @@ TokenQueue* lex (char* text)
  
     /* compile regular expressions */
     Regex* whitespace = Regex_new("^[ \n]");
-    Regex* space = Regex_new("^[ ]");
+    Regex* space = Regex_new("^[ \t]");
 
-    Regex* symbols = Regex_new("^\\(|\\)|\\+|\\*|\\-|\\!|==|\\=|\\||\\?|\\:|\\;|\\{|\\}");
+    Regex* symbols = Regex_new("^\\(|\\)|\\+|\\*|\\-|\\!|\\=|\\||\\?|\\:|\\;|\\{|\\}|(\\>\\=)|(\\<\\=)|(\\=\\=)");
     Regex* int_constants = Regex_new("^0|[1-9]+[0-9]*");
     Regex* identifiers = Regex_new("^[a-zA-Z][0-9a-zA-Z_]*");
 
     Regex* hex = Regex_new("^0x[a-fA-F0-9]+");
     Regex* string_literals = Regex_new("^\\\"[^\'\"]*\\\"");
-    Regex* valid_keywords = Regex_new("^def|^if|^else|^while|^break|^continue|^int|^bool|^void|^true|^false");
+    Regex* valid_keywords = Regex_new("^def|^if|^else|^while|^break|^continue|^int|^bool|^void|^true|^false([^a-zA-Z0-9])");
     Regex* invalid_keywords = Regex_new("^for|^callout|^class|^interface|^extends|^implements|^new|^string|^float|^double|^null");
-    Regex* comments = Regex_new("// [0-9a-zA-Z_]*");
+    Regex* comments = Regex_new("//.*");
 
     /* read and handle input */
     char match[MAX_TOKEN_LEN];
@@ -45,6 +45,12 @@ TokenQueue* lex (char* text)
             }
         } else if (Regex_match(space, text, match)) {
 
+        } else if (Regex_match(valid_keywords, text, match)) {
+            TokenQueue_add(tokens, Token_new(KEY, match, placeholder));
+
+        } else if (Regex_match(invalid_keywords, text, match)) {
+            Error_throw_printf("Invalid token!\n");
+
         } else if (Regex_match(identifiers, text, match)) {
             TokenQueue_add(tokens, Token_new(ID, match, placeholder));
             
@@ -59,12 +65,6 @@ TokenQueue* lex (char* text)
 
         } else if (Regex_match(int_constants, text, match)) {
             TokenQueue_add(tokens, Token_new(DECLIT, match, placeholder));
-
-        } else if (Regex_match(valid_keywords, text, match)) {
-            TokenQueue_add(tokens, Token_new(KEY, match, placeholder));
-            
-        } else if (Regex_match(invalid_keywords, text, match)) {
-            Error_throw_printf("Invalid token!\n");
 
         } else if (Regex_match(comments, text, match)) {
 
