016cdd56a8cf49d251972001933f453fbf82cb7f
Joselyne M. Tran <tran3jm@dukes.jmu.edu>
2022-09-15 01:37:12 -0400

    All test working kinda??

--- p1/p1-lexer/src/p1-lexer.c
+++ p1/p1-lexer/src/p1-lexer.c
@@ -35,16 +35,15 @@ TokenQueue* lex (char* text)
     /* compile regular expressions */
     Regex* whitespace = Regex_new("^[ \n]");
     Regex* space = Regex_new("^[ \t]");
-
-    Regex* symbols = Regex_new("^[\\(|\\)|\\+|\\*|\\-|\\!|\\=|\\||\\?|\\:|\\;|\\{|\\}|(\\>\\=)|(\\<\\=)|(\\=\\=)]");
+    Regex* symbols = Regex_new("^((\\&{2})|\\|{2}|\\={1,2}|<=|>=|\\!\\=|\\/|"
+        "\\(|\\)|\\+|\\*|\\-|\\!|\\=|\\,|\\?|\\:|\\;|\\{|\\}|(\\=\\=)|\\%|\\[|\\]|<|>)");
     Regex* int_constants = Regex_new("^0|[1-9]+[0-9]*");
     Regex* identifiers = Regex_new("^[a-zA-Z][0-9a-zA-Z_]*");
-
     Regex* hex = Regex_new("^0x[a-fA-F0-9]+");
     Regex* string_literals = Regex_new("^\\\"[^\'\"]*\\\"");
     Regex* valid_keywords = Regex_new("^\\b(def|if|else|while|break|return|continue|int|bool|void|true|false)\\b");
     Regex* invalid_keywords = Regex_new("^for|^callout|^class|^interface|^extends|^implements|^new|^string|^float|^double|^null");
-    Regex* comments = Regex_new("//.*");
+    Regex* comments = Regex_new("\\/\\/.*");
 
     /* read and handle input */
     char match[MAX_TOKEN_LEN];
@@ -66,6 +65,9 @@ TokenQueue* lex (char* text)
                 placeholder++;
             }
         } else if (Regex_match(space, text, match)) {
+
+        } else if (Regex_match(comments, text, match)) {
+            // Comments are useless, *he says in a comment* i agree *she also says in a comment*
 
         } else if (Regex_match(valid_keywords, text, match)) {
             TokenQueue_add(tokens, Token_new(KEY, match, placeholder));
@@ -99,8 +101,6 @@ TokenQueue* lex (char* text)
         } else if (Regex_match(int_constants, text, match)) {
             TokenQueue_add(tokens, Token_new(DECLIT, match, placeholder));
 
-        } else if (Regex_match(comments, text, match)) {
-            // Comments are useless, *he says in a comment*
         } else {
             freeRegex(whitespace, space, symbols, int_constants, identifiers, hex,\
                 string_literals, valid_keywords, invalid_keywords, comments);
